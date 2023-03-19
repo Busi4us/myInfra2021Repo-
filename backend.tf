439ba4f221b853ed1a658da4bbe34b0070bf6d08
Busi4us <113306408+Busi4us@users.noreply.github.com>
2023-03-19 18:50:12 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
-terraform {
-  backend "s3" {
-    bucket = "terr-jenk-state-bucket"
-    key = "main"
-    region = "us-east-2"
-    dynamodb_table = "Teraform-lock"
-  }
+# terraform {
+#   backend "s3" {
+#     bucket = "terr-jenk-state-bucket"
+#     key = "main"
+#     region = "us-east-2"
+#     dynamodb_table = "Teraform-lock"
+#   }
   required_providers{
     aws = {
       source = "hashicorp/aws"
