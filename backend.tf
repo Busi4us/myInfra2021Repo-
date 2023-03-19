17e09c4ee84158ab0b73924adc568d7e37658314
Busi4us <113306408+Busi4us@users.noreply.github.com>
2023-03-19 16:10:06 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -5,4 +5,10 @@ terraform {
     region = "us-east-2"
     dynamodb_table = "Teraform-lock"
   }
+  required_providers{
+    aws = {
+      source = "hashicorp/aws"
+      version = "> 4.0"
+    }
+  }
 }
